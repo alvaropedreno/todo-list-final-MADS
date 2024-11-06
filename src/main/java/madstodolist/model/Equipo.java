9565e7617d91ed1454fd22507aabec144104e97f
Mercedes Suárez Canga <msc162@alu.ua.es>
2024-11-06 18:31:04 +0100

    Segundo test - Añadir y buscar equipo en la base de datos

--- src/main/java/madstodolist/model/Equipo.java
+++ src/main/java/madstodolist/model/Equipo.java
@@ -1,11 +1,34 @@
 package madstodolist.model;
 
-public class Equipo {
+import javax.persistence.*;
+import java.io.Serializable;
 
+@Entity
+@Table(name = "equipos")
+
+public class Equipo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String nombre;
+
+    // Constructor vacío necesario para JPA/Hibernate.
+    // No debe usarse desde la aplicación.
+    public Equipo() {}
 
     public Equipo(String nombre) {
         this.nombre = nombre;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getNombre() {
