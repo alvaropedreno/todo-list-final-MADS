86134adb2fedc822fbe1c30092636f0bec189566
Mercedes Suárez Canga <msc162@alu.ua.es>
2024-11-07 13:34:39 +0100

    Septimo test - Metodo de servicio para el listado de equipos

--- src/main/java/madstodolist/repository/EquipoRepository.java
+++ src/main/java/madstodolist/repository/EquipoRepository.java
@@ -7,4 +7,6 @@ import java.util.List;
 
 public interface EquipoRepository extends CrudRepository<Equipo, Long> {
     public List<Equipo> findAll();
+
+    List<Equipo> findAllByOrderByNombreAsc();
 }
