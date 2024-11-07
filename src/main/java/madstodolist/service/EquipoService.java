2c5ae4449947afc95b6bd714317a9551c584ed73
Mercedes Suárez Canga <msc162@alu.ua.es>
2024-11-07 13:39:40 +0100

    Octavo test - Metodos de servicio para añadir y recuperar usuarios a un equipo

--- src/main/java/madstodolist/service/EquipoService.java
+++ src/main/java/madstodolist/service/EquipoService.java
@@ -1,8 +1,11 @@
 package madstodolist.service;
 
 import madstodolist.dto.EquipoData;
+import madstodolist.dto.UsuarioData;
 import madstodolist.model.Equipo;
+import madstodolist.model.Usuario;
 import madstodolist.repository.EquipoRepository;
+import madstodolist.repository.UsuarioRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,6 +23,9 @@ public class EquipoService {
     EquipoRepository equipoRepository;
 
     @Autowired
+    UsuarioRepository usuarioRepository;
+
+    @Autowired
     private ModelMapper modelMapper;
 
     @Transactional
@@ -35,6 +41,22 @@ public class EquipoService {
         return modelMapper.map(equipo, EquipoData.class);
     }
 
+    @Transactional
+    public void añadirUsuarioAEquipo(Long id, Long id1) {
+        Equipo equipo = equipoRepository.findById(id).orElse(null);
+        Usuario usuario = usuarioRepository.findById(id1).orElse(null);
+        equipo.addUsuario(usuario);
+    }
+
+    @Transactional(readOnly = true)
+    public List<UsuarioData> usuariosEquipo(Long id) {
+        Equipo equipo = equipoRepository.findById(id).orElse(null);
+        // Hacemos uso de Java Stream API para mapear la lista de entidades a DTOs.
+        return equipo.getUsuarios().stream()
+                .map(usuario -> modelMapper.map(usuario, UsuarioData.class))
+                .collect(Collectors.toList());
+    }
+
     @Transactional(readOnly = true)
     public List<EquipoData> findAllOrdenadoPorNombre() {
         List<Equipo> equipos = equipoRepository.findAllByOrderByNombreAsc();
@@ -42,5 +64,7 @@ public class EquipoService {
                 .map(equipo -> modelMapper.map(equipo, EquipoData.class))
                 .collect(Collectors.toList());
     }
+
+
 
 }
